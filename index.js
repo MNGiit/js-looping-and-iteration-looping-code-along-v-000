f8c426ebbb9f18f05a69c0907bab950b36f7744b
Mohammed Noori <kmanoorib@gmail.com>
2019-02-22 22:41:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,11 +12,3 @@ function tailsNeverFails() {
 
 
 
-
-
-
-invokes Math.random() to simulate coin flips ‣
-ReferenceError: tailsNeverFails is not defined
-    at Context.it (test/indexTest.js:51:4)spy.returns(0.2);
-tailsNeverFails();
-expect(spy.calledOnce).to.be.true;

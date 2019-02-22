459a659f214f597eed0ef8142bd5b0d366927e0d
Mohammed Noori <kmanoorib@gmail.com>
2019-02-22 22:37:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+function printBadges(arr) {
+  for (let number = 1; number <= arr.length; number++) {
+    console.log()
+  }
+}
+
+
+printBadges()
+invokes console.log() once for each element in the passed-in array ‣
+ReferenceError: printBadges is not defined
+    at Context.it (test/indexTest.js:16:4)printBadges(['Lisa', 'Kaitlin', 'Jan']);
+expect(spy.callCount).to.eq(3);

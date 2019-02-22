44726e8c8b625b2dedd3859ed620e5dbc50dc2aa
Mohammed Noori <kmanoorib@gmail.com>
2019-02-22 22:40:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 // Code your solutions in this file
 function printBadges(arr) {
   for (let number = 0; number < arr.length; number++) {
-    console.log(`${arr[number]}`);
+    console.log(`Welcome ${arr[number]}! You are employee #${number+1}.`);
   }
 }
-
-
-
